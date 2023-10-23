e68fda909c27f0095a74cbaceae94a95340ce56e
qc1128qc@outlook.com <1799953029@qq.com>
2023-10-24 01:33:33 +0800

    Mountain Tai Reactivities

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,21 +1,32 @@
-
+import {  useEffect, useState } from 'react';
 import './App.css'
+import DuckItem from './DuckItem'
 import { ducks } from './demo'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
- 
+  const [activities, setActivities] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+  
   return (
     <div>
       <h1>Reactivities</h1>
-      {ducks.map(duck =>(
-        <div key={duck.name}>
-          <span>{duck.name}</span> 
-          <button onClick={() => duck.makeSound(duck.name + ' quack!')}>Make Sound</button>   
-        </div>
-      ))}
+      <Header as='h2' icon='users' content='Mountain Tai Reactivities'/>
+      <List>
+        {activities.map((activity: any) => (
+        <List.Item key={activity.id}>
+         {activity.title}
+         </List.Item>
+        ))}
+      </List>
     </div>
-    
-    
   )
 }
 
